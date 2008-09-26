f3a623e7707181bfea4d1d57d2716d9f2e976ce0
dariusz.mikulski <dariusz.mikulski@6d04e58a-8b45-11dd-a9db-710176c1da70>
2008-09-26 19:40:31 +0000

    add cmakelists and first file in microblog library

---
+++ trunk/microblog/ConfigurationTO.cpp
@@ -0,0 +1,76 @@
+/*******************************************************************************
+* This file is part of twittoid.
+*  Maintained by : Dariusz Mikulski <dariusz.mikulski@gmail.com>
+*  Authored By  : Dariusz Mikulski <dariusz.mikulski@gmail.com>
+*
+*  PlexyDesk is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  PlexyDesk is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with twittoid. If not, see <http://www.gnu.org/licenses/lgpl.html>
+*******************************************************************************/
+
+#include <QString>
+
+#include "ConfigurationTO.h"
+
+namespace MicroBlogEngine
+{
+    class ConfigurationTO::Private
+    {
+        public:
+            Private() {}
+            ~Private() {}
+            
+            QString m_login;
+            QString m_password;
+            QString m_websiteUrl;
+    };
+    
+    
+    ConfigurationTO::ConfigurationTO()
+        : d(new Private())
+    {
+    }
+
+    ConfigurationTO::~ConfigurationTO()
+    {
+    }
+    
+    QString ConfigurationTO::getLogin()
+    {
+        return d->m_login;
+    }
+
+    void ConfigurationTO::setLogin(QString val)
+    {
+        d->m_login = val;
+    }
+        
+    QString ConfigurationTO::getPassword()
+    {
+        return d->m_password;
+    }
+
+    void ConfigurationTO::setPassword(QString val)
+    {
+        d->m_password = val;
+    }
+
+    QString ConfigurationTO::getWebsiteUrl()
+    {
+        return d->m_websiteUrl;
+    }
+
+    void ConfigurationTO::setWebsiteUrl(QString val)
+    {
+        d->m_websiteUrl = val;
+    }
+};
