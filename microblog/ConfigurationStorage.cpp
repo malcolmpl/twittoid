8db218d030e229080c69af78814a2ee14c63313e
dariusz.mikulski <dariusz.mikulski@6d04e58a-8b45-11dd-a9db-710176c1da70>
2008-09-27 15:54:55 +0000

    config storage files

---
+++ microblog/ConfigurationStorage.cpp
@@ -0,0 +1,39 @@
+/*******************************************************************************
+* This file is part of twittoid.
+*  Maintained by : Dariusz Mikulski <dariusz.mikulski@gmail.com>
+*  Authored By  : Dariusz Mikulski <dariusz.mikulski@gmail.com>
+*
+*  PlexyDesk is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  PlexyDesk is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with twittoid. If not, see <http://www.gnu.org/licenses/lgpl.html>
+*******************************************************************************/
+
+#include "ConfigurationStorage.h"
+
+namespace MicroBlogEngine
+{
+    class ConfigurationStorage::Private
+    {
+        public:
+            Private() {}
+            ~Private() {}
+    };
+    
+    ConfigurationStorage::ConfigurationStorage()
+        : d(new Private())
+    {
+    }
+
+    ConfigurationStorage::~ConfigurationStorage()
+    {
+    }
+};
