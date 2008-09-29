df05265d774fe330c686440865aa47af767ac8b5
bymenda <bymenda@6d04e58a-8b45-11dd-a9db-710176c1da70>
2008-09-29 15:55:44 +0000

    added pluginloader class

---
+++ trunk/microblog/PluginLoader.h
@@ -0,0 +1,29 @@
+/*******************************************************************************
+* This file is part of twittoid.
+*  Maintained by : Dariusz Mikulski <dariusz.mikulski@gmail.com>
+*  Authored By  : Rafael Muñoz Cárdenas <bymenda@gmail.com>
+*
+*  Twittoid is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  Twittoid is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with twittoid. If not, see <http://www.gnu.org/licenses/lgpl.html>
+*******************************************************************************/
+
+#ifndef PLUGINLOADER_H_
+#define PLUGINLOADER_H_
+
+class PluginLoader
+{
+    PluginLoader ();
+    ~PluginLoader ();
+};
+
+#endif
