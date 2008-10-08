32811da90c01cc0818c2117bc7c045eaf4056bf2
bymenda <bymenda@6d04e58a-8b45-11dd-a9db-710176c1da70>
2008-10-08 22:08:18 +0000

    gnu headers corrected

--- trunk/microblog/MicroblogItem.cpp
+++ trunk/microblog/MicroblogItem.cpp
@@ -3,12 +3,12 @@
 *  Maintained by : Dariusz Mikulski <dariusz.mikulski@gmail.com>
 *  Authored By  : Dariusz Mikulski <dariusz.mikulski@gmail.com>
 *
-*  PlexyDesk is free software: you can redistribute it and/or modify
+*  Twittoid is free software: you can redistribute it and/or modify
 *  it under the terms of the GNU Lesser General Public License as published by
 *  the Free Software Foundation, either version 3 of the License, or
 *  (at your option) any later version.
 *
-*  PlexyDesk is distributed in the hope that it will be useful,
+*  Twittoid is distributed in the hope that it will be useful,
 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 *  GNU Lesser General Public License for more details.
